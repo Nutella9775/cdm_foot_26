8a3a48ab314d11488e82be368616969a01b28f87
Adrien Barbet <adrienbarbet7@gmail.com>
2026-05-20 22:32:12 +0200

    Score match #1 : Canada 5-1 Maroc

--- scores.js
+++ scores.js
@@ -1,2 +1,363 @@
-// Scores CDM 2026 – mis à jour automatiquement par l'admin
-const matchScores = [];
+// Scores CDM 2026 – mis à jour automatiquement
+const matchScores = [
+  {
+    "id": 1,
+    "score1": 5,
+    "score2": 1
+  },
+  {
+    "id": 2,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 3,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 4,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 5,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 6,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 7,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 8,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 9,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 10,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 11,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 12,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 13,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 14,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 15,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 16,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 17,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 18,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 19,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 20,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 21,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 22,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 23,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 24,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 25,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 26,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 27,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 28,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 29,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 30,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 31,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 32,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 33,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 34,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 35,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 36,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 37,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 38,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 39,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 40,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 41,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 42,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 43,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 44,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 45,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 46,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 47,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 48,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 49,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 50,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 51,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 52,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 53,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 54,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 55,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 56,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 57,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 58,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 59,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 60,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 61,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 62,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 63,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 64,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 65,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 66,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 67,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 68,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 69,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 70,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 71,
+    "score1": null,
+    "score2": null
+  },
+  {
+    "id": 72,
+    "score1": null,
+    "score2": null
+  }
+];
