34fbb92b9d1ac56744ae90a6ed81bd2075024487
Adrien Barbet <adrienbarbet7@gmail.com>
2026-05-25 09:50:39 +0200

    Ajout pronostics coucou

--- players.js
+++ players.js
@@ -1,27 +1,10 @@
 let players = [
   {
-    "name": "Bonjour",
+    "name": "coucou",
     "bets": {
       "1": {
-        "bet1": 5,
-        "bet2": 1
-      }
-    }
-  },
-  {
-    "name": "jaune",
-    "bets": {
-      "1": {
-        "bet1": 8,
-        "bet2": 4
-      },
-      "2": {
         "bet1": 2,
-        "bet2": 3
-      },
-      "3": {
-        "bet1": 2,
-        "bet2": 8
+        "bet2": 2
       }
     }
   }
